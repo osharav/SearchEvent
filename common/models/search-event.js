a535af23fcb0aab4437c9f0ec2f2af360e60ff29
tamartwe <tamar.twena@gmail.com>
2017-12-10 21:32:44 +0200

    add remote method

--- common/models/search-event.js
+++ common/models/search-event.js
@@ -1,5 +1,23 @@
 'use strict';
 
-module.exports = function(Searchevent) {
+module.exports = function(SearchEvent) {
+
+    SearchEvent.findWithFilter = function(filter, cb) {
+        var query = JSON.parse(filter);
+        SearchEvent.find(query, function(err, results) {
+            if(err) {
+                return cb(err);
+            }
+            return cb(err, results);
+        });
+        
+      }
+  
+    SearchEvent.remoteMethod('findWithFilter', {
+            accepts: {arg: 'filter', type: 'string'},
+            returns: {arg: 'listObjects', type: 'string'},
+            http: { verb: 'get', path : '/findWithFilter'}
+
+    });
 
 };
